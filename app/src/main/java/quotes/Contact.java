a2f37fd4eb6f489dc30b4fbafc2e0ce09c588348
suhibabbas <sohaib_salah96@yahoo.com>
2022-03-10 14:43:44 +0300

    the user can search by author name and an word, and the test is working

---
+++ app/src/main/java/quotes/Contact.java
@@ -0,0 +1,25 @@
+package quotes;
+
+public class Contact {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+
+    public Contact(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
